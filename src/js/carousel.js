6023e6cfe1553afde05d6e5200d2b9ecae5a90e9
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-01-22 08:40:18 -0700

    Added componentDidUpdate function to verify next carousel selection

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -19,4 +19,24 @@ class Carousel extends React.Component
             currentItemIndex: this.state.currentItemIndex + amount
         });
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot)
+    {
+        const numberChildren = React.Children.toArray(this.props.children).length;
+        if (this.props.children !== prevProps.children)
+        {
+            this.oldItemIndex = null;
+        }
+        let newPosition = this.state.currentItemIndex % numberChildren;
+        while (newPosition < 0)
+        {
+            newPosition += numberChildren;
+        }
+        if (this.state.currentItemIndex !== newPosition)
+        {
+            this.setState({
+                currentItemIndex: newPosition
+            });
+        }
+    }
 }
