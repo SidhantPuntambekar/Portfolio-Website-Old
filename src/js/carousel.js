17eaa8ce25176bf357662107835a3b2f14ce585c
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-01-22 08:36:52 -0700

    Added movePosition function to Carousel.js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,22 @@
 import React from "react";
 import "../styles/Carousel.css";
 
+class Carousel extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            currentItemIndex: 0
+        };
+        this.oldItemIndex = null;
+    }
 
+    movePosition(amount)
+    {
+        this.oldItemIndex = this.state.currentItemIndex;
+        this.setState({
+            currentItemIndex: this.state.currentItemIndex + amount
+        });
+    }
+}
