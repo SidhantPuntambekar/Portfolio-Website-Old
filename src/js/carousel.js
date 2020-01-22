0301090441d34adfb87193db2b48ef87128ee957
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-01-22 08:41:54 -0700

    Adapted render method from SaurabhTotey

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -39,4 +39,34 @@ class Carousel extends React.Component
             });
         }
     }
+
+    render() {
+		const id = `carousel${this.props.title}`;
+		const makeCarouselControls = () => (
+			<fieldset className={"carouselControlBox"} aria-label={"carousel controls"} aria-controls={id}>
+				<button className={"carouselControl"} onClick={() => this.movePosition(-1)} aria-label={"previous"}>&#10094;</button>
+				<button className={"carouselControl"} onClick={() => this.movePosition(1)} aria-label={"next"}>&#10095;</button>
+			</fieldset>
+		);
+		return <div className={"carousel"} id={id} aria-live={"polite"}>
+			<h3>{this.props.title}</h3>
+			{makeCarouselControls()}
+			<ul className={"carouselContentList"}>{
+				React.Children.toArray(this.props.children).map((child, index) => {
+					let childContainerClassName = this.state.currentItemIndex === index ? "" : "hidden";
+					let style = {};
+					if (this.oldItemIndex !== null && [this.state.currentItemIndex, this.oldItemIndex].includes(index)) {
+						childContainerClassName = "animation";
+						style["--animation-name"] = index === this.state.currentItemIndex ? "fadeIn" : "fadeOut";
+					}
+					return <li aria-hidden={index !== this.state.currentItemIndex} className={"carouselItem"} key={index}>
+						<div className={childContainerClassName} style={style}>{child}</div>
+					</li>
+				})
+			}</ul>
+			{makeCarouselControls()}
+		</div>
+	}
 }
+
+export default Carousel
