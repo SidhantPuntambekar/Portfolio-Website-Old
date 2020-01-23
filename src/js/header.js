03db97c62a69e55d831d834dc46bb4e0a99304a0
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-01-23 09:29:43 -0700

    Added header.js

---
+++ src/js/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "../styles/Header.css";
+
+export default () => 
+(
+    <header>
+            <div id = {"title-cotainer"}>
+                <h1 id = {"title"}>Sidhant Puntambekar</h1>
+            </div>
+            <Navbar />
+    </header>
+)
